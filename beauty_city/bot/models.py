25f9540cb3fd16c1ec29ce82fb148d78335be256
Jana <yanix2x2@gmail.com>
2025-06-25 15:01:36 +0300

    add db.sql

--- beauty_city/bot/models.py
+++ beauty_city/bot/models.py
@@ -1,21 +1,110 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Salon(models.Model):
-    pass
+    address = models.CharField(max_length=100, verbose_name='Адрес салона')
 
-
-class Master(models.Model):
-    pass
+    def __str__(self):
+        return self.address
 
 
 class Service(models.Model):
-    pass
+    TREATMENT_CHOICES = [
+        ('Hair', 'Стилист'),
+        ('Nail', 'Ноготочки'),
+        ('Makeup', 'Макияж')
+    ]
+    
+    treatment = models.CharField(
+        max_length=10,
+        choices=TREATMENT_CHOICES,
+        verbose_name='Тип услуги'
+    )
+    price = models.DecimalField(max_digits=6, decimal_places=0, verbose_name='Цена')
 
+    def __str__(self):
+        return f'{self.get_treatment_display()} {self.price}р.'
+
+
+class Master(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Имя мастера')
+    salon = models.ForeignKey(
+        Salon,
+        verbose_name='Салон',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    services = models.ManyToManyField(
+        Service,
+        verbose_name='Услуги мастера'
+    )
+
+    def __str__(self):
+        if self.salon:
+            return f"{self.name} из «{self.salon.address}»"
+        return "Мастер без салона"
+ 
 
 class Client(models.Model):
-    pass
+    tg_id = models.BigIntegerField(unique=True, verbose_name="Telegram ID")
+    name = models.CharField(max_length=50, verbose_name='Имя клиента')
+    phonenumber = PhoneNumberField(max_length=12, verbose_name='Номер телефона')
+
+    def __str__(self):
+        return self.name
 
 
 class Registration(models.Model):
-    pass
+    salon = models.ForeignKey(
+        Salon,
+        verbose_name='Салон',
+        on_delete=models.CASCADE
+    )
+    master = models.ForeignKey(
+        Master,
+        verbose_name='Мастер',
+        on_delete=models.CASCADE
+    )
+    client = models.ForeignKey(
+        Client,
+        verbose_name='Клиент',
+        on_delete=models.CASCADE
+    )
+    service = models.ForeignKey(
+        Service,
+        verbose_name='Услуга',
+        on_delete=models.CASCADE
+    )
+    service_date = models.DateField(
+        verbose_name='Дата процедуры'
+    )
+    slot = models.CharField(
+        max_length=13,
+        verbose_name='Время записи',
+        default=''
+    )
+    reminder_sent = models.BooleanField(
+        default=False,
+        verbose_name='Отправилось напоминание'
+    )
+    registration_time = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Когда произведена бронь'
+    )
+
+    def __str__(self):
+        return f"Бронь №{self.pk} на {self.service_date} в {self.slot}"
+
+
+# Registration.objects.create(
+#     salon=salon,
+#     master=master,
+#     client=client,
+#     service=hair,
+#     service_date="2025-06-26",
+#     slot="10:00-11:00"
+# ) Так создаём регистрацию с конкретной услугой мастера
+
+
